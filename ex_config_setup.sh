b46c7aabb9df7ba9c28b99037592473dc38ef8ad
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2017-05-24 16:22:37 +0200

    Update ex_config_setup.sh

--- ex_config_setup.sh
+++ ex_config_setup.sh
@@ -4,6 +4,6 @@ launch.sh
 curl -Lo Dockerfile https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_Dockerfile
 curl -Lo deployment.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_deployment.yaml
 curl -Lo secret.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_secret.yaml
-curl -Lo configmap.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_configmap.yaml
+curl -Lo nginx.conf https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_nginx.conf
 curl -Lo service.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_service.yaml
 curl -Lo index.html https://raw.githubusercontent.com/sgreben/k8s-training/master/ex_config_index.html
