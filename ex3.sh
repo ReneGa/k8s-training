7f69e86aa28de437cd93d42565890d5c6e626302
ReneGa <ReneGa@users.noreply.github.com>
2017-05-24 11:38:05 +0200

    Update ex3.sh

--- ex3.sh
+++ ex3.sh
@@ -4,3 +4,4 @@ launch.sh
 curl -Lo Dockerfile https://raw.githubusercontent.com/sgreben/k8s-training/master/ex3_Dockerfile
 curl -Lo deployment.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex3_deployment.yaml
 curl -Lo service.yaml https://raw.githubusercontent.com/sgreben/k8s-training/master/ex3_service.yaml
+curl -Lo index.html https://raw.githubusercontent.com/sgreben/k8s-training/master/ex3_index.html
